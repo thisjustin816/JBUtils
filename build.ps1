e823aa7bc5bddb3ac6571729888747eca95fde74
Justin Beeson <justinbeeson@gmail.com>
2024-05-31 11:00:35 -0700

    Rev version to build using updated scripts

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $BuildPSModule = @{
     Name        = 'JBUtils'
-    Version     = '1.0.7'
+    Version     = '1.0.8'
     Description = 'A module with functions for various basic/low-level tasks.'
     Tags        = ('PSEdition_Desktop', 'PSEdition_Core', 'Windows')
 }
